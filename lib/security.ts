74b7d95d683f1c2684c7fe594aa37f3f8d86c57f
profbruuno <96679152+profbruuno@users.noreply.github.com>
2025-09-26 14:54:06 +0300

    security.ts

---
+++ lib/security.ts
@@ -0,0 +1,9 @@
+export function encryptKey(key: string, password: string) {
+  // TODO: Implement client-side encryption
+  return key;
+}
+
+export function decryptKey(ciphertext: string, password: string) {
+  // TODO: Implement server-side decryption
+  return ciphertext;
+}
