a8ea6455ed94c4d722960dd57023d5f9accf4dca
profbruuno <96679152+profbruuno@users.noreply.github.com>
2025-09-26 21:03:21 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
-  // Vercel serverless function configuration
-  experimental: {
-    serverComponentsExternalPackages: ['crypto-js'],
-  },
 }
 
 module.exports = nextConfig
