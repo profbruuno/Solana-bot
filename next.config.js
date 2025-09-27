ac9c7c9cf0fd466307421173e86fe5ef0c4a8592
profbruuno <96679152+profbruuno@users.noreply.github.com>
2025-09-27 06:30:28 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Disable server-side features for static export
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
